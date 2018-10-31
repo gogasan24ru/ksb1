7b6cb657af7ea4a9573b104c9aada1c48112529f
gogasan24ru <a@gogasan.tk>
2018-10-31 13:14:10 +0300

    add menu

--- static/template.php
+++ static/template.php
@@ -3,9 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <title>Title</title>
+    <script type="text/css" src="/static/style.css" >
+
+    </script>
 </head>
 <body>
 <div class="menu">
+    <?php if ($authed) {?>
+        <a href="?module=listTasks">List tasks</a>
+        <a href="?module=auth&logout=1">Logout</a>
+    <?php } ?>
+    <a href="?module=auth">Login</a>
+    <a href="?module=editTask">Submit new</a>
 </div>
 <?php echo $contents; ?>
 </body>
