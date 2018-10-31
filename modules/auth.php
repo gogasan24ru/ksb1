dadd677f4c70a06b4f307c740b4eb5f7b2ea28e3
gogasan24ru <a@gogasan.tk>
2018-10-31 13:11:34 +0300

    basic implementation

--- modules/auth.php
+++ modules/auth.php
@@ -1,1 +1,30 @@
 <?php
+
+class auth
+{
+    function render(){
+        $result="";
+        $am=new authManager();
+        if($am->authResult()=="")
+        {
+            $result=$this->renderAuthForm();
+        }
+        return $result;
+
+    }
+    function renderAuthForm()
+    {
+        return
+            '
+            <form method="POST" >
+            <input type="hidden" name="module" value="auth">
+            <input type="text" name="username">
+            <input type="password" name="password">
+            <input type="submit" value="Login">
+            
+            
+            </form>
+            
+            ';
+    }
+}
