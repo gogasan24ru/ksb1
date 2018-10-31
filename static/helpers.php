09b0b1bd1de4d7da5090692a1bf98747c55561dd
gogasan24ru <a@gogasan.tk>
2018-10-31 14:24:41 +0300

    add db singleton, complete auth management

--- static/helpers.php
+++ static/helpers.php
@@ -1,7 +1,37 @@
 <?php
+class helperSigleton
+{
+    private static $instance;
+    private static $db;
 
+    private function __construct($cnf)
+    {
+        self::$db = new SafeMySQL(
+            array('user' => $cnf['username'],
+                'pass' => $cnf['password'],
+                'db' => $cnf['database'],
+                'host'=>$cnf['host'],
+                'charset' => 'utf8')
+        );
 
-class authManager
+    }
+    public static function getInstance($cnf=null)
+    {
+        if(!isset($cnf))return self::$instance;
+        if (!isset(self::$instance))
+        {
+            self::$instance = new helperSigleton($cnf);
+        }
+        return self::$instance;
+    }
+
+    public static function getDB()
+    {
+        return self::$db;
+    }
+}
+
+class authManagerHelper
 {
     function authResult(){
         if(isset($_SESSION['username']))
@@ -12,6 +42,20 @@ class authManager
         {
             return "";
         }
+    }
+    function auth($username, $password)
+    {
+        session_start();
+        $data=helperSigleton::getInstance()->getDB()->getOne("SELECT id,username FROM users WHERE password=?s AND username=?s",md5($password),$username);
+        if($data==false)return false;
+        $_SESSION['username']=$data['username'];
+        return true;
+    }
+    function logout()
+    {
+        unset($_SESSION['username']);
+        session_destroy();
+
     }
 
 }
