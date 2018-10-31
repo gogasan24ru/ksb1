f5a61288522e7d9b0a7adc4bb44ddc6f11cddfc7
gogasan24ru <a@gogasan.tk>
2018-10-31 13:10:14 +0300

    basic implementation of mvc-router-like engine

--- index.php
+++ index.php
@@ -1,4 +1,30 @@
 <?php
+//setup
+require_once "static/functions.php";
+require_once "static/config.php";
+require_once "static/mysql.php";
 
+
+if(isset($_GET['module']))
+{
+    $db = new SafeMySQL(
+        array('user' => $mycnf['username'],
+            'pass' => $mycnf['password'],
+            'db' => $mycnf['database'],
+            'host'=>$mycnf['host'],
+            'charset' => 'utf8')
+    );
+
+    $module=$_GET['module'];
+    include "modules/".$module.'.php';
+    $worker = new $module;
+    $contents=$worker->render();
+    include("static/template.php");
+}
+else{
+    $contents="Welcome";
+    include("static/template.php");
+
+}
 
 ?>
